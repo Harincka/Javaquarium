dac0b9f314424327b335bd53d44b9bb61c8e0af3
johann <johannengrand59@gmail.com>
2017-11-30 13:51:07 +0100

    Ajout du mappage hibernate avec l'UserDO

--- src/com/javaquarium/beans/data/UserDO.java
+++ src/com/javaquarium/beans/data/UserDO.java
@@ -7,12 +7,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
+@Entity
+@Table(name="User")
 public class UserDO {
 
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id")
 	private Integer id;
+	
+	@Column(name= "login")
 	private String login;
+	
+	@Column(name= "password")
 	private String password;
 	
 	public Integer getId() {

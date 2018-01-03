e4befa23cef14bbe4b6641d7a1525a99d63cab8b
johann <johannengrand59@gmail.com>
2018-01-03 22:15:24 +0100

    Modification du PoissonDAO pour être compatible avec les fichiers en lien avec la table Aquarium

--- src/com/javaquarium/dao/PoissonDAO.java
+++ src/com/javaquarium/dao/PoissonDAO.java
@@ -1,6 +1,5 @@
 package com.javaquarium.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Transaction;
@@ -29,18 +28,32 @@ public class PoissonDAO implements IPoissonDAO {
 	}
 	
 	public List<PoissonDO> getAll(){
+		
 		Session s = HibernateUtils.getSession();
 		
-		Transaction tx = s.beginTransaction();
+		try {
+			
+			Query q = s.createQuery("from PoissonDO");
+			return (List<PoissonDO>) q.list();
+			
+		} finally {
+			s.close();
+		}
+					
+	}
+	
+	public PoissonDO getPoisson(String name) {
+		Session s = HibernateUtils.getSession();
+		PoissonDO poisson = null;
 		
-		Query q = s.createQuery("from PoissonDO");
-		ArrayList<PoissonDO> list = (ArrayList) q.list();
+		Query q = s.createQuery("from Poisson where nom= :nomPoisson");
+		q.setString("nomPoisson", name);
 		
-		tx.commit();
+		poisson = (PoissonDO) q.uniqueResult();
+		
 		s.close();
 		
-		return list;
-		
+		return poisson;
 	}
 
 }
